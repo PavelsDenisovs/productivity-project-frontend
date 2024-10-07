0438f5a4b78fee0de8f5fd44b3d1449e8e7d6fae
Pavel Denisov <pavelsdenisovs1@gmail.com>
2024-10-07 15:10:35 +0300

    Create navbar

--- src/components/features/MainNavBar/MainNavBar.tsx
+++ src/components/features/MainNavBar/MainNavBar.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './MainNavBar.module.scss';
+import Link from 'next/link';
 
 const MainNavBar: React.FC = () => {
   const routes = ['messages', 'profile', 'posts'];
 
   return (
     <nav className={styles.nav}>
-      {routes.map((route) => (
-        <div className={styles.route} key='route'>{route}</div>
-      ))}
+      <div className={styles.router}>
+        {routes.map((route) => (
+          <Link href={`/${route}`} className={styles.route} key={route}>
+            {route}
+          </Link>
+        ))}
+      </div>
     </nav>
   )
 }
